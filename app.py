b4198f7102831f2cca04bf51c7fb24a8ed196952
Abhijoy Sarkar <abhijoy.sar@gmail.com>
2023-11-03 14:47:18 +0100

    Loading realtime added

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def main():
     uploaded_file = st.file_uploader("Upload your CSV", type="csv")
 
     if uploaded_file:
-        skills_taxonomy, individual_skills = extract_skills_from_csv_in_realtime(uploaded_file)
+        skills_taxonomy, current_df = extract_skills_from_csv_in_realtime(uploaded_file)
 
         st.write("## Skills Taxonomy:")
         for skill in skills_taxonomy:
@@ -20,8 +20,7 @@ def main():
 
         # Convert DataFrame to a CSV in memory
         buf = io.StringIO()
-        df = pd.DataFrame(list(individual_skills.items()), columns=['Name', 'Skills'])
-        df.to_csv(buf, index=False)
+        current_df.to_csv(buf, index=False)
         csv_data = buf.getvalue()
 
         # Provide download button for the CSV
@@ -31,6 +30,7 @@ def main():
             file_name="processed_data.csv",
             mime="text/csv"
         )
+
 
 if __name__ == "__main__":
     main()
