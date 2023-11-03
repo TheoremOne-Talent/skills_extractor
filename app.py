1e97742c27342f6fac858029375583f805b4e867
Abhijoy Sarkar <abhijoy.sar@gmail.com>
2023-11-03 14:52:28 +0100

    Fixing rerun bug with state

--- app.py
+++ app.py
@@ -9,18 +9,30 @@ def main():
 
     uploaded_file = st.file_uploader("Upload your CSV", type="csv")
 
-    if uploaded_file:
+    # Check if 'processed' state exists in session state, if not, initialize it to False
+    if 'processed' not in st.session_state:
+        st.session_state.processed = False
+
+    # If a new file is uploaded, process the data and set the 'processed' state to True
+    if uploaded_file and not st.session_state.processed:
         skills_taxonomy, current_df = extract_skills_from_csv_in_realtime(uploaded_file)
 
+        st.session_state.skills_taxonomy = skills_taxonomy
+        st.session_state.current_df = current_df
+
+        st.session_state.processed = True
+
+    # If the data has been processed, display the results and download button
+    if st.session_state.processed:
         st.write("## Skills Taxonomy:")
-        for skill in skills_taxonomy:
+        for skill in st.session_state.skills_taxonomy:
             st.write(skill)
 
         st.write("\n## Individual Skills:")
 
         # Convert DataFrame to a CSV in memory
         buf = io.StringIO()
-        current_df.to_csv(buf, index=False)
+        st.session_state.current_df.to_csv(buf, index=False)
         csv_data = buf.getvalue()
 
         # Provide download button for the CSV
